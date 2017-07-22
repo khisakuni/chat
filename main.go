f157da911bb53b3b8113ab0e1244833e780d0eb8
khisakuni <kohei@gophilosophie.com>
2017-07-22 12:06:47 -0700

    WIP

--- main.go
+++ main.go
@@ -25,9 +25,10 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// root
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
-
+	http.Handle("/room", r)
+	go r.run()
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
